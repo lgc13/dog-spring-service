9d425844f0ddfa324c663c0732e927ce83a00e78
Lucas Costa <lucas.gomes.costa@gmail.com>
2020-02-01 15:05:02 -0600

    initial commit

---
+++ src/main/java/com/lucas/dogspringservice/DogSpringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.dogspringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogSpringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogSpringServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/dogspringservice/DogSpringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.dogspringservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogSpringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
