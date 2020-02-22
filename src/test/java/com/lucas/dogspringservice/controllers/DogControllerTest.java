4fb033021c52b0de0d3d1b4c745b9b4a2ebb41d2
Lucas Costa <lucas.gomes.costa@gmail.com>
2020-02-22 16:04:37 -0600

    refactored tests

--- src/test/java/com/lucas/dogspringservice/controllers/DogControllerTest.java
+++ src/test/java/com/lucas/dogspringservice/controllers/DogControllerTest.java
@@ -3,6 +3,7 @@ package com.lucas.dogspringservice.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lucas.dogspringservice.entity.Dog;
 import com.lucas.dogspringservice.services.DogsService;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -27,22 +28,28 @@ class DogControllerTest {
     @MockBean
     private DogsService dogsService;
 
-    @Test
-    public void getDogById_whenDogIsFoundByIdProvided_returnsDog() throws Exception {
-        Dog dog = new Dog("Sasha", "blue");
-        when(dogsService.getDogById(1)).thenReturn(dog);
+    @Nested
+    class getDogById {
+        @Test
+        public void whenDogIsFoundByIdProvided_returnsDog() throws Exception {
+            Dog dog = new Dog("Sasha", "blue");
+            when(dogsService.getDogById(1)).thenReturn(dog);
 
-        mockMvc.perform(get("/dog/1"))
-                .andExpect(content().string(objectMapper.writeValueAsString(dog)));
+            mockMvc.perform(get("/dog/1"))
+                    .andExpect(content().string(objectMapper.writeValueAsString(dog)));
+        }
     }
 
-    @Test
-    public void getAllDogs_whenDogsFound_returnsListOfDogs() throws Exception {
-        Dog dog = new Dog("Sasha", "blue");
-        List<Dog> dogList = List.of(dog);
-        when(dogsService.getAllDogs()).thenReturn(dogList);
+    @Nested
+    class getAllDogs {
+        @Test
+        public void getAllDogs_whenDogsFound_returnsListOfDogs() throws Exception {
+            Dog dog = new Dog("Sasha", "blue");
+            List<Dog> dogList = List.of(dog);
+            when(dogsService.getAllDogs()).thenReturn(dogList);
 
-        mockMvc.perform(get("/dog/all"))
-                .andExpect(content().string(objectMapper.writeValueAsString(dogList)));
+            mockMvc.perform(get("/dog/all"))
+                    .andExpect(content().string(objectMapper.writeValueAsString(dogList)));
+        }
     }
 }
