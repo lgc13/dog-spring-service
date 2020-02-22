82cba5cb040258c12b711bbd0d18cf2e4352d4bb
Lucas Costa <lucas.gomes.costa@gmail.com>
2020-02-21 23:13:20 -0600

    added embedded h2 db for testing, and some notes about it

--- src/test/java/com/lucas/dogspringservice/DogSpringServiceApplicationTests.java
+++ src/test/java/com/lucas/dogspringservice/DogSpringServiceApplicationTests.java
@@ -1,13 +1,14 @@
 package com.lucas.dogspringservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase // this replaces dataSource bean with embedded version
 class DogSpringServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
