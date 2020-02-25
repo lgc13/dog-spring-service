2a821936caa8d47c09f049dd7b9647db30b95f4b
Lucas Costa <lucas.gomes.costa@gmail.com>
2020-02-24 22:49:07 -0600

    removed SSLMODE

--- src/main/java/com/lucas/dogspringservice/config/CommonConfiguration.java
+++ src/main/java/com/lucas/dogspringservice/config/CommonConfiguration.java
@@ -1,22 +1,21 @@
 package com.lucas.dogspringservice.config;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 
 @Configuration
 public class CommonConfiguration {
 
     @Bean
-    public BasicDataSource dataSource() throws URISyntaxException       {
+    public BasicDataSource dataSource() throws URISyntaxException {
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setUrl(dbUrl);
